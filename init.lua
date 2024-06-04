87354308cbab0d88aeb4215b8c4179c1f5df2cbd
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-06-04 19:33:16 +0200

    tweaaks

--- init.lua
+++ init.lua
@@ -1,7 +1,4 @@
 require('alex.options')
 require('alex.keymaps')
-require('alex.lazy')
 require('alex.autocommands')
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+require('alex.lazy')
