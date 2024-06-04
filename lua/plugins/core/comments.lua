0e8a0a6d46960880a6d3de50033c69a2bf06ddcc
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-06-04 18:57:24 +0200

    refac

--- lua/plugins/core/comments.lua
+++ lua/plugins/core/comments.lua
@@ -1,5 +1,9 @@
 -- "gc" to comment visual regions/lines
 return {
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
+  -- comment code
   'numToStr/Comment.nvim',
   lazy = false,
 
--- lua/plugins/core/motions.lua
+++ lua/plugins/core/motions.lua
@@ -1,10 +1,5 @@
 return {
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   {
-
     'unblevable/quick-scope',
     lazy = false,
     init = function()
@@ -12,5 +7,14 @@ return {
       vim.api.nvim_set_hl(0, 'QuickScopePrimary', { fg = '#e37933', underline = true, ctermfg = 155 })
       vim.api.nvim_set_hl(0, 'QuickScopeSecondary', { fg = '#e8274b', underline = true, ctermfg = 81 })
     end,
+  },
+
+  {
+    'ggandor/leap.nvim',
+    keys = {
+      { 'U', '<Plug>(leap-backward)', mode = { 'n', 'x', 'o' } },
+      { 'E', '<Plug>(leap-forward)', mode = { 'n', 'x', 'o' } },
+      { 'ge', '<Plug>(leap-from-window)', mode = { 'n', 'x', 'o' } },
+    },
   },
 }
