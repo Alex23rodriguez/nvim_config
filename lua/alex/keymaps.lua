29008b2aaf126430b74118d0d0c84fb3ac1156cb
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-06-04 16:49:18 +0200

    fixed enter in quickfix

--- lua/alex/keymaps.lua
+++ lua/alex/keymaps.lua
@@ -71,7 +71,7 @@ set('n', '[q', '<cmd>cprev<CR>', { desc = 'Previous Quickfix item' })
 set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Set buffer diagnostics to location list' })
 
 -- Function to toggle the quickfix window
 set('n', 'qq', function()
