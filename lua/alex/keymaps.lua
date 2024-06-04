1d7389bb28bbd72e42a47ed932f9ba7eebfaebd9
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-06-04 14:09:52 +0200

    minimum keymaps

--- lua/alex/keymaps.lua
+++ lua/alex/keymaps.lua
@@ -4,13 +4,15 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+local set = vim.keymap.set
+
+set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -18,19 +20,86 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- [[ Colemak movement ]]
+-- hjkl
+set('', 'u', 'k', { desc = 'Move up with u' })
+set('', 'n', 'h', { desc = 'Move left with n' })
+set('', 'e', 'j', { desc = 'Move down with e' })
+set('n', 'i', 'l', { desc = 'Move right with i' }) -- keep visual mode as 'inside'
+
+-- remap conflicts
+set('n', '<CR>', 'i', { desc = 'Insert with enter' })
+set('', 'j', 'e', { desc = 'Map j to end' })
+set('', 'k', 'n', { desc = 'k for next in search' })
+set('', 'K', 'N', { desc = 'K for previous in search' })
+
+set('n', 'z', 'u', { desc = 'undo with z' })
+set('n', 'Z', '<C-r>', { desc = 'redo with z' })
+
+set('n', 'S', 'z', { desc = 'Vim z prefix' })
+
+--
+
+-- [[ Window management ]]
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+set('n', '<C-e>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+set('n', '<C-u>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+set('n', '<C-n>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+set('n', '<C-i>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+set('n', '<C-j>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+
+set('n', 's', '<c-w>', { desc = 'window management' })
+set('n', 'sx', '<c-w>x', { desc = 'Swap window with nth window' })
+set('n', 'sh', '<c-w>s', { desc = 'Split window horizontally' })
+set('n', 'sv', '<c-w>v', { desc = 'Split window vertically' }) -- redundant
+set('n', 'sd', '<c-w>d', { desc = 'Split and go to definition' }) -- redundant
+set('n', 'sgt', '<c-w>gt', { desc = 'Go to next tab' }) -- redundant
+set('n', 'sgT', '<c-w>gT', { desc = 'Go to previous tab' }) -- redundant
+set('n', 'sO', '<c-w>o', { desc = 'Close all but current window' })
+set('n', 'sq', '<c-w>q', { desc = ':q' }) -- redundant
+set('n', 'sz', '<c-w>z', { desc = 'Close preview window' }) -- redundant
+
+-- Alternative (for when ctrl is not going through the app)
+-- set('n', 'sE', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- set('n', 'sU', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- set('n', 'sN', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- set('n', 'sI', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+
+-- [[ Buffer management ]]
+set('n', 'ss', '<cmd>b#<CR>', { desc = 'Switch to previous buffer' })
+set('n', 'sn', '<cmd>bp<CR>', { desc = 'Left buffer' })
+set('n', 'si', '<cmd>bn<CR>', { desc = 'Right buffer' })
+set('n', 'sw', '<cmd>bd<CR>', { desc = 'Close all but current buffer' })
+set('n', 'sW', '<cmd>%bd|e#<CR>', { desc = 'Close all but current buffer' })
+
+-- [[ Quickfix ]]
+set('n', ']q', '<cmd>cnext<CR>', { desc = 'Next Quickfix item' })
+set('n', '[q', '<cmd>cprev<CR>', { desc = 'Previous Quickfix item' })
+-- Function to toggle the quickfix window
+set('n', 'qq', function()
+  if vim.api.nvim_get_option_value('filetype', {}) == 'qf' then
+    -- Close the quickfix window
+    vim.cmd('cclose')
+  else
+    -- Open the quickfix window
+    vim.cmd('copen')
+  end
+end, { desc = 'Toggle Quickfix' })
+
+-- [[ Misc ]]
+set('v', '<', '<gv', { desc = 'Stay in visual mode when indenting' })
+set('v', '>', '>gv', { desc = 'Stay in visual mode when indenting' })
+
+-- when pasting over something, don't override register
+set('v', 'p', '"_dP', { desc = 'Visual paste does not override register' })
