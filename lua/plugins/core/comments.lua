03496cee5787f61640d3fe627b4597f9d036644c
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-06-04 20:13:49 +0200

    added docs plugin

--- lua/plugins/core/comments.lua
+++ lua/plugins/core/comments.lua
@@ -27,4 +27,12 @@ return {
   opts = {
     mappings = { basic = true, extra = false },
   },
+  {
+    'kkoomen/vim-doge',
+    config = function()
+      vim.api.nvim_set_var('doge_doc_standard_python', 'google')
+      vim.api.nvim_set_var('doge_enable_mappings', false)
+      vim.keymap.set('n', '<Leader>D', '<Plug>(doge-generate)')
+    end,
+  },
 }
