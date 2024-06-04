b80cbcc80a58f381311764cd5e9d10e3be3737c9
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-06-04 18:51:00 +0200

    added quickscope to core plugins

--- lua/plugins/core/core.lua
+++ lua/plugins/core/core.lua
@@ -3,4 +3,14 @@ return {
   'tpope/vim-sleuth',
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+
+    'unblevable/quick-scope',
+    lazy = false,
+    init = function()
+      vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
+      vim.api.nvim_set_hl(0, 'QuickScopePrimary', { fg = '#e37933', underline = true, ctermfg = 155 })
+      vim.api.nvim_set_hl(0, 'QuickScopeSecondary', { fg = '#e8274b', underline = true, ctermfg = 81 })
+    end,
+  },
 }
