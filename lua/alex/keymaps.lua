dc1644fd10a59a074d6e71dd87319c5fa732d997
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-06-04 20:39:51 +0200

    get rid of double escape to exit terminal

--- lua/alex/keymaps.lua
+++ lua/alex/keymaps.lua
@@ -100,7 +100,8 @@ set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
 -- set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
