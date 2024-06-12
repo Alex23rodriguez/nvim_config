7e082cf31f7195ff57f02792429b6a56d432a9d0
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-06-12 14:29:53 +0200

    added buffer_manager plugin

---
+++ lua/plugins/core/buffers.lua
@@ -0,0 +1,18 @@
+return {
+  'j-morano/buffer_manager.nvim',
+  keys = {
+    {
+      '<leader>b',
+      function()
+        require('buffer_manager.ui').toggle_quick_menu()
+      end,
+    },
+  },
+  config = function()
+    vim.api.nvim_set_hl(0, 'BufferManagerModified', { fg = '#ffaaaa' })
+    require('buffer_manager').setup({
+      width = 0.8,
+      -- short_file_names = false,
+    })
+  end,
+}
