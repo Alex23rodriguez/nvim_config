f4b67552e402af8bc1cc01676d4ae92267bd4670
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-08-02 11:22:20 +0200

    disable flash for f and t

--- lua/plugins/core/motions.lua
+++ lua/plugins/core/motions.lua
@@ -21,7 +21,7 @@ return {
     'folke/flash.nvim',
     event = 'VeryLazy',
     ---@type Flash.Config
-    opts = {},
+    opts = { modes = { char = { enabled = false } } },
     -- stylua: ignore
     keys = {
       { "E",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
