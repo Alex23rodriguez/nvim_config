fd2793ae362501e6331a018ef33d3f72b1499f02
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-08-02 11:05:40 +0200

    added flash.nvim

--- lua/plugins/core/motions.lua
+++ lua/plugins/core/motions.lua
@@ -17,6 +17,21 @@ return {
   --     { 'ge', '<Plug>(leap-from-window)', mode = { 'n', 'x', 'o' } },
   --   },
   -- },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "E",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "U",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
+  },
+
   'jeetsukumaran/vim-indentwise',
   {
     'ThePrimeagen/harpoon',
