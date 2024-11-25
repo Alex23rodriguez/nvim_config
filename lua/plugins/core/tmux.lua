bec818e93b3f51877e61415554dd2c51f65f30ea
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2024-11-25 14:52:50 +0100

    comment out tmux

--- lua/plugins/core/tmux.lua
+++ lua/plugins/core/tmux.lua
@@ -1,30 +1,31 @@
-return {
-  'aserowy/tmux.nvim',
-  config = function()
-    require('tmux').setup({
-      copy_sync = {
-        enable = false,
-      },
-      navigation = {
-        enable_default_keybindings = false,
-      },
-      resize = {
-        enable_default_keybindings = false,
-        resize_step_x = 5,
-        resize_step_y = 5,
-      },
-    })
-
-    require('tmux.keymaps').register('n', {
-      ['<A-n>'] = "<cmd>lua require('tmux').move_left()<cr>",
-      ['<A-j>'] = "<cmd>lua require('tmux').move_right()<cr>",
-      ['<A-u>'] = "<cmd>lua require('tmux').move_top()<cr>",
-      ['<A-e>'] = "<cmd>lua require('tmux').move_bottom()<cr>",
-
-      ['<AS-n>'] = "<cmd>lua require('tmux').resize_left()<cr>",
-      ['<AS-j>'] = "<cmd>lua require('tmux').resize_right()<cr>",
-      ['<AS-u>'] = "<cmd>lua require('tmux').resize_top()<cr>",
-      ['<AS-e>'] = "<cmd>lua require('tmux').resize_bottom()<cr>",
-    })
-  end,
-}
+return {}
+-- return {
+--   'aserowy/tmux.nvim',
+--   config = function()
+--     require('tmux').setup({
+--       copy_sync = {
+--         enable = false,
+--       },
+--       navigation = {
+--         enable_default_keybindings = false,
+--       },
+--       resize = {
+--         enable_default_keybindings = false,
+--         resize_step_x = 5,
+--         resize_step_y = 5,
+--       },
+--     })
+--
+--     require('tmux.keymaps').register('n', {
+--       ['<A-n>'] = "<cmd>lua require('tmux').move_left()<cr>",
+--       ['<A-j>'] = "<cmd>lua require('tmux').move_right()<cr>",
+--       ['<A-u>'] = "<cmd>lua require('tmux').move_top()<cr>",
+--       ['<A-e>'] = "<cmd>lua require('tmux').move_bottom()<cr>",
+--
+--       ['<AS-n>'] = "<cmd>lua require('tmux').resize_left()<cr>",
+--       ['<AS-j>'] = "<cmd>lua require('tmux').resize_right()<cr>",
+--       ['<AS-u>'] = "<cmd>lua require('tmux').resize_top()<cr>",
+--       ['<AS-e>'] = "<cmd>lua require('tmux').resize_bottom()<cr>",
+--     })
+--   end,
+-- }
