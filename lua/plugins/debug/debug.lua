9d7b38bcb1f80f888c5b32e2ed2fc6712a9d81e9
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2024-11-26 10:15:42 +0100

    updated and simplified dap

--- lua/plugins/debug/debug.lua
+++ lua/plugins/debug/debug.lua
@@ -1,112 +1,48 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-
 return {
-  -- NOTE: Yes, you can install new plugins here!
-  'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
-  dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
+  {
+    -- STANDARD
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'anuvyklack/hydra.nvim',
+    },
+    config = function()
+      local dap = require('dap')
 
-    -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
-
-    -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
-
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
-  },
-  config = function()
-    local dap = require('dap')
-    local dapui = require('dapui')
-
-    require('mason-nvim-dap').setup({
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_installation = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        -- more here: https://github.com/jay-babu/mason-nvim-dap.nvim/blob/main/lua/mason-nvim-dap/mappings/source.lua
-        -- 'delve', -- go
-        'python', --python
-      },
-    })
-
-    -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<leader>di', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<leader>de', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<leader>du', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>dn', dap.step_back, { desc = 'Debug: Step Back' })
-
-    vim.keymap.set('n', '<leader>dE', dap.down, { desc = 'Debug: Down' })
-    vim.keymap.set('n', '<leader>dU', dap.step_back, { desc = 'Debug: Up' })
-
-    vim.keymap.set('n', '<leader>dr', dap.repl.open, { desc = 'Debug: REPL' })
-    vim.keymap.set('n', '<leader>dp', dap.pause, { desc = 'Debug: Pause' })
-    vim.keymap.set('n', '<leader>dk', dap.terminate, { desc = 'Debug: Kill' })
-
-    vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>dB', function()
-      dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
-    end, { desc = 'Debug: Set Breakpoint with condition' })
-
-    -- dapui
-    vim.keymap.set('n', '<leader>dg', function()
-      require('dapui').toggle()
-    end, { desc = 'Debug: toggle UI' })
-
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup({
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
+      local Hydra = require('hydra')
+      Hydra({
+        name = 'Debug Mode',
+        mode = { 'n' },
+        body = '<leader>d',
+        config = {
+          invoke_on_body = true,
+          color = 'pink',
+          hint = { border = 'rounded' },
         },
-      },
-    })
+        heads = {
+          { 'c', dap.continue, { desc = 'Start/Continue', nowait = true } },
+          { 'i', dap.step_over, { desc = 'Step Over', nowait = true } },
+          { 'e', dap.step_into, { desc = 'Step Into', nowait = true } },
+          { 'u', dap.step_out, { desc = 'Step Out', nowait = true } },
+          { 'n', dap.step_back, { desc = 'Step Back', nowait = true } },
 
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+          { 'E', dap.down, { desc = 'Down', nowait = true } },
+          { 'U', dap.step_back, { desc = 'Up', nowait = true } },
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+          { 'r', dap.repl.open, { desc = 'REPL', nowait = true } },
+          { 'p', dap.pause, { desc = 'Pause', nowait = true } },
+          { 'k', dap.terminate, { desc = 'Kill', nowait = true } },
 
-    -- Install golang specific config
-    require('dap-go').setup({
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has('win32') == 0,
-      },
-    })
-  end,
+          { 'b', dap.toggle_breakpoint, { desc = 'Toggle Breakpoint', nowait = true } },
+          {
+            'B',
+            function()
+              dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
+            end,
+            { desc = 'Debug: Set Breakpoint with condition', nowait = true },
+          },
+        },
+      })
+    end,
+  },
+  -- install language specific adapters in language folder
 }
--- lua/plugins/python/debug.lua
+++ lua/plugins/python/debug.lua
@@ -1,4 +1,10 @@
 return {
-  "mfussenegger/nvim-dap-python",
+  'mfussenegger/nvim-dap-python',
   ft = 'python',
+  config = function()
+    local dap_python = require('dap-python')
+    -- setup takes a python executable with debugpy installed
+    -- will respect venv
+    dap_python.setup('python')
+  end,
 }
