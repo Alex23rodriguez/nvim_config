1aa1e0ab6662829915fa13f077873a4a8b3e9c0b
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2025-01-12 17:43:33 +0100

    added virtual-text to dap

--- lua/plugins/debug/debug.lua
+++ lua/plugins/debug/debug.lua
@@ -5,4 +5,11 @@ return {
   },
   -- keymaps defined in hydras.lua
   -- install language specific adapters in language folder
+
+  -- virtual text
+  {
+    'theHamsta/nvim-dap-virtual-text',
+    opts = { commented = true, virt_text_pos = 'eol' },
+    -- config = true,
+  },
 }
