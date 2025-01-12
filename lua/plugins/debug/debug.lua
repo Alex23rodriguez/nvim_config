e86dc0294cee2936c83165d0e31d563da075b48c
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2025-01-12 18:00:55 +0100

    added persistent breakpoints to dap

--- lua/plugins/debug/debug.lua
+++ lua/plugins/debug/debug.lua
@@ -12,4 +12,10 @@ return {
     opts = { commented = true, virt_text_pos = 'eol' },
     -- config = true,
   },
+
+  -- persistent breakpionts
+  {
+    'Weissle/persistent-breakpoints.nvim',
+    opts = { load_breakpoints_event = { 'BufReadPost' } },
+  },
 }
