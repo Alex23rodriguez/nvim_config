be987e0c9debbc1ad60a66866c05c50d61a98f6a
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2025-01-14 14:21:40 +0100

    add 'Launch from directory' python dap config

--- lua/plugins/python/debug.lua
+++ lua/plugins/python/debug.lua
@@ -5,6 +5,17 @@ return {
     local dap_python = require('dap-python')
     -- setup takes a python executable with debugpy installed
     -- will respect venv
+    require('dap').configurations.python = {
+      {
+        type = 'python',
+        request = 'launch',
+        name = 'Launch from directory',
+        program = '${file}',
+        cwd = function()
+          return vim.fn.input('Basepath: ', vim.fn.getcwd(), 'dir')
+        end,
+      },
+    }
     dap_python.setup('python')
   end,
 }
