e33b7198b33580351ba46e3a9494c63309ade7c0
Alejandro Rodriguez <alejandro.rodriguez@partner.bmwgroup.com>
2025-09-05 09:44:50 +0000

    tweaks: changed tree-sitter-context configs

--- lua/plugins/core/view.lua
+++ lua/plugins/core/view.lua
@@ -3,9 +3,18 @@ return {
   lazy = false,
   keys = {
     { '<leader>k', '<cmd>TSContextToggle<cr>', { desc = 'Toggle Treesitter Context' } },
+    {
+      '[c',
+      function()
+        require('treesitter-context').go_to_context(vim.v.count1)
+      end,
+      { desc = 'Jump up [n] contexts' },
+    },
   },
-  config = true,
-  -- opts = {
-  -- separator = '-',
-  -- },
+  -- config = true,
+  opts = {
+    max_lines = 20,
+    multiline_threshold = 8,
+    separator = '-',
+  },
 }
