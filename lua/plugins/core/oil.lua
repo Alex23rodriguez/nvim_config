8a8bb969e41bed00f8f49295d6d8f2c029b48ac8
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2025-09-10 08:46:28 +0200

    docs: added description to oil commands

--- lua/plugins/core/oil.lua
+++ lua/plugins/core/oil.lua
@@ -33,63 +33,90 @@ return {
       ['Y'] = 'actions.preview_scroll_down',
       ['L'] = 'actions.preview_scroll_up',
       ['g?'] = 'actions.show_help',
-      ['gi'] = function()
-        require('oil').set_columns({
-          { 'permissions', highlight = '@attribute' },
-          { 'mtime', format = '%Y-%m-%d %H:%M', highlight = '@function' },
-          { 'size', highlight = '@keyword' },
-          'icon',
-        })
-      end,
+      ['gv'] = {
+        function()
+          require('oil').set_columns({
+            { 'permissions', highlight = '@attribute' },
+            { 'mtime', format = '%Y-%m-%d %H:%M', highlight = '@function' },
+            { 'size', highlight = '@keyword' },
+            'icon',
+          })
+        end,
+        desc = 'View: verbose',
+      },
       -- sorting and view options
-      ['gn'] = function()
-        require('oil').set_columns({ 'icon' })
-        require('oil').set_sort({ { 'type', 'asc' } })
-      end,
-      ['gs'] = function()
-        require('oil').set_columns({ { 'size', highlight = '@keyword' }, 'icon' })
-        require('oil').set_sort({ { 'type', 'asc' }, { 'size', 'desc' } })
-      end,
-      ['gm'] = function()
-        require('oil').set_columns({
-          { 'mtime', format = 'mod: %Y-%m-%d %H:%M', highlight = '@function' },
-          'icon',
-        })
-        require('oil').set_sort({ { 'type', 'asc' }, { 'mtime', 'desc' } })
-      end,
-      ['gc'] = function()
-        require('oil').set_columns({
-          { 'ctime', format = 'created: %Y-%m-%d %H:%M', highlight = '@function' },
-          'icon',
-        })
-        require('oil').set_sort({ { 'type', 'asc' }, { 'ctime', 'desc' } })
-      end,
-      ['ga'] = function()
-        require('oil').set_columns({
-          { 'atime', format = 'accessed: %Y-%m-%d %H:%M', highlight = '@function' },
-          'icon',
-        })
-        require('oil').set_sort({ { 'type', 'asc' }, { 'atime', 'desc' } })
-      end,
+      ['gn'] = {
+        function()
+          require('oil').set_columns({ 'icon' })
+          require('oil').set_sort({ { 'type', 'asc' } })
+        end,
+        desc = 'View: normal',
+      },
+      ['gs'] = {
+        function()
+          require('oil').set_columns({ { 'size', highlight = '@keyword' }, 'icon' })
+          require('oil').set_sort({ { 'type', 'asc' }, { 'size', 'desc' } })
+        end,
+        desc = 'View: size',
+      },
+      ['gm'] = {
+        function()
+          require('oil').set_columns({
+            { 'mtime', format = 'mod: %Y-%m-%d %H:%M', highlight = '@function' },
+            'icon',
+          })
+          require('oil').set_sort({ { 'type', 'asc' }, { 'mtime', 'desc' } })
+        end,
+        desc = 'View: modified',
+      },
+      ['gc'] = {
+        function()
+          require('oil').set_columns({
+            { 'ctime', format = 'created: %Y-%m-%d %H:%M', highlight = '@function' },
+            'icon',
+          })
+          require('oil').set_sort({ { 'type', 'asc' }, { 'ctime', 'desc' } })
+        end,
+        desc = 'View: created',
+      },
+      ['ga'] = {
+        function()
+          require('oil').set_columns({
+            { 'atime', format = 'accessed: %Y-%m-%d %H:%M', highlight = '@function' },
+            'icon',
+          })
+          require('oil').set_sort({ { 'type', 'asc' }, { 'atime', 'desc' } })
+        end,
+        desc = 'View: accessed',
+      },
       -- marks
-      ['m'] = function()
-        local mark = vim.fn.input('save as: ')
-        if string.len(mark) ~= nil then
-          _OIL_MARKS[mark] = require('oil').get_current_dir()
-          print("saved '" .. mark .. "'")
-        end
-      end,
-      ['M'] = function()
-        vim.notify(vim.inspect(_OIL_MARKS))
-      end,
-      ["'"] = function()
-        local mark = vim.fn.input('load: ')
-        if _OIL_MARKS[mark] ~= nil then
-          require('oil').open(_OIL_MARKS[mark])
-        else
-          print("'" .. mark .. "' not found")
-        end
-      end,
+      ['m'] = {
+        function()
+          local mark = vim.fn.input('save as: ')
+          if string.len(mark) ~= nil then
+            _OIL_MARKS[mark] = require('oil').get_current_dir()
+            print("saved '" .. mark .. "'")
+          end
+        end,
+        desc = 'Mark: save current directory',
+      },
+      ['M'] = {
+        function()
+          vim.notify(vim.inspect(_OIL_MARKS))
+        end,
+        desc = 'Mark: list',
+      },
+      ["'"] = {
+        function()
+          local mark = vim.fn.input('load: ')
+          if _OIL_MARKS[mark] ~= nil then
+            require('oil').open(_OIL_MARKS[mark])
+          else
+            print("'" .. mark .. "' not found")
+          end
+        end,
+        desc = 'Mark: open directory',
+      },
     },
     view_options = { show_hidden = true },
   },
