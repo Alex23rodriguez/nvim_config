d9ae99f79036823bf90238b942e28aa768be08e8
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2025-10-10 17:16:45 +0200

    format: replace black with ruff

--- lua/plugins/core/format.lua
+++ lua/plugins/core/format.lua
@@ -32,7 +32,7 @@ return { -- Autoformat
         lua = { 'stylua' },
         toml = { 'tombi' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'black', 'isort' },
+        python = { 'ruff_format', 'isort' },
         -- You can tell conform to run *until* a formatter is found.
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
