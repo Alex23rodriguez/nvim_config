f65c382072f0da5bde2b431e4ec2bc4006cf1f6f
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2025-11-24 06:02:09 +0100

    feat: refactor plugins

---
+++ lua/plugins/core/refactor.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'ThePrimeagen/refactoring.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    lazy = false,
+    opts = {},
+  },
+  {
+    {
+      'alexpasmantier/pymple.nvim', -- when renaming a file, asks to update the import paths
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'MunifTanjim/nui.nvim',
+        -- optional (nicer ui)
+        'stevearc/dressing.nvim',
+        'nvim-tree/nvim-web-devicons',
+      },
+      build = ':PympleBuild',
+      config = function()
+        require('pymple').setup()
+      end,
+    },
+  },
+}
