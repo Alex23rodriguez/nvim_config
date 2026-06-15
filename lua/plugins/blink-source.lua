e4ff9a8125e0d7d164ea7712cb694107950f633f
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2026-06-15 18:17:37 +0200

    tweak: show autocomplete source

---
+++ lua/plugins/blink-source.lua
@@ -0,0 +1,16 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      menu = {
+        draw = {
+          columns = {
+            { "source_name" },
+            { "kind_icon" },
+            { "label", "label_description", gap = 1 },
+          },
+        },
+      },
+    },
+  },
+}
