034044630cfbf2a599946af4c60e0713842fafa4
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2026-06-16 10:54:13 +0200

    tweak: dbt keymaps

--- lua/plugins/dbt.lua
+++ lua/plugins/dbt.lua
@@ -5,10 +5,13 @@ return {
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
     ft = "sql",
     keys = {
-      { "<leader>Dc", ":DbtCompile<CR>" },
-      { "<leader>Dr", ":DbtRun<CR>" },
-      { "<leader>DR", ":DbtRunFull<CR>" },
-      { "<leader>Dt", ":DbtTest<CR>" },
+      { "<leader>Dc", ":DbtCompile<CR>", desc = "dbt Compile" },
+      { "<leader>Dr", ":DbtRun<CR>", desc = "dbt Run current model" },
+      { "<leader>DR", ":DbtRunFull<CR>", desc = "dbt Run current model (full)" },
+      { "<leader>Dt", ":DbtTest<CR>", desc = "dbt Test" },
+      { "<leader>DU", ":DbtListUpstreamModels<CR>", desc = "dbt List upstream models" },
+      { "<leader>DD", ":DbtListDownstreamModels<CR>", desc = "dbt List downstream models" },
+      { "<leader>Dy", ":DbtModelYaml<CR>", desc = "dbt Generate yaml for current model" },
     },
   },
   {
